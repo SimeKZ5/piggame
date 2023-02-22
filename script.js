11df15b3520f4b28170fe49385bb6c56cede802e
Domagoj Šimatović <124262236+SimeKZ5@users.noreply.github.com>
2023-02-22 12:49:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,103 @@
 'use strict';
+//  elementi
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Pocetne tocke
+// score0El.textContent = 0;
+// score1El.textContent = 0;
+// diceEl.classList.add('hidden');
+
+// const scores = [0, 0];
+// let currentScore = 0;
+// let activePlayer = 0;
+// let playing = true;
+
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  player0El.classList.remove('player-winner');
+  player1El.classList.remove('player-winner');
+  player0El.classList.add('player-active');
+  player1El.classList.remove('player-active');
+  diceEl.classList.add('hidden');
+};
+
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+//Rolling
+
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    // Generirat dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // pokazati sliku
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // ako je 1: if true, prebaciti na drugog igraca
+    if (dice !== 1) {
+      // Dodaj vrijednost kocke u rezultat(score)
+      // currentScore = currentScore + dice;
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // prebaci na drugog igraca
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // Zbrajanje rezultata
+    // scores[1] = scores[1] + currentScore
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // Ako je >= 100
+    if (scores[activePlayer] >= 100) {
+      // Zavrsi igru
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player-winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // Prebaci na drugog igraca ako nije
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', init);
